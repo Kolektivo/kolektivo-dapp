d263b3c730dfd56a5b93946b6a3c42c85fbd95e6
Pawel Janicki <paweljanicki256@gmail.com>
2022-06-02 20:27:07 +0800

    Add main github workflow

--- test/e2e/integration/example.spec.js
+++ test/e2e/integration/example.spec.js
@@ -4,3 +4,10 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+describe("Testing Test", () => {
+  it("App is deployed on vercel.com subdomain", () => {
+    cy.visit("/");
+    cy.url().should("include", ".vercel.app");
+  });
+});
