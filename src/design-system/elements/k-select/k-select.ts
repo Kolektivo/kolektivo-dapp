16ef717f9692421c7a62dfa597e1b89f638b746b
Chad Cartwright <vipergtsrz@gmail.com>
2022-06-22 10:34:48 -0500

    Add some select stuff and change scrollbar styles

--- src/design-system/elements/k-select/k-select.ts
+++ src/design-system/elements/k-select/k-select.ts
@@ -1,9 +1,12 @@
 import { CustomElement, ICustomElementViewModel, bindable } from 'aurelia';
 export class KSelect implements ICustomElementViewModel {
   @bindable options = [];
-  @bindable allowEmpty = true;
+  @bindable placeholder: string;
   constructor() {
     // you can inject the element or any DI in the constructor
+  }
+  changed() {
+    (document.activeElement as HTMLElement).blur();
   }
 }
 (CustomElement.getDefinition(KSelect) as { capture: boolean }).capture = true;
