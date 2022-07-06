84c0bbc30456e165b948f6bf93dac8b916761b1a
Pawel Janicki <paweljanicki256@gmail.com>
2022-07-06 12:45:12 +0800

    Fix linting errors

--- src/pages/swap/swap.ts
+++ src/pages/swap/swap.ts
@@ -3,5 +3,5 @@ import { ICustomElementViewModel } from 'aurelia';
 export class Swap implements ICustomElementViewModel {
   constructor() {
     // you can inject the element or any DI in the constructor
-   }
+  }
 }
