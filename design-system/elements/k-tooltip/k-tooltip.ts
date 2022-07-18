58b6f669b56cc5685749dcc19dc4bcd79d32beff
Chad Cartwright <vipergtsrz@gmail.com>
2022-07-18 16:18:59 -0500

    showing the issues with the current eslint rules

--- design-system/elements/k-tooltip/k-tooltip.ts
+++ design-system/elements/k-tooltip/k-tooltip.ts
@@ -1,19 +1,26 @@
 import { ICustomElementViewModel, bindable } from 'aurelia';
 import { numberToPixels } from './../../common';
 export class KTooltip implements ICustomElementViewModel {
-  @bindable message: string;
-  @bindable({ set: numberToPixels }) top = 0;
-  @bindable({ set: numberToPixels }) bottom = 0;
+  @bindable message:
+
+  string;
+  @bindable({ set: numberToPixels }) top
+
+    = 0;
+  @bindable({ set: numberToPixels,
+  }) bottom = 0;
   @bindable({ set: numberToPixels }) right = 0;
   @bindable({ set: numberToPixels }) left = 0;
   constructor(private readonly element: HTMLElement) {
     // you can inject the element or any DI in the constructor
   }
 
-  get style() {
+  get style () {
     return {
-      top: this.top,
-      left: this.left,
+      top:
+      this.top,
+      left: this.left
+      ,
     };
   }
 }
