cf970fd25e97c4d1f83ddbc04587c9410a4125fc
Pawel Janicki <paweljanicki256@gmail.com>
2022-08-04 12:36:44 +0800

    Value card

--- src/pages/treasury/pages/overview/value-card/value-card.ts
+++ src/pages/treasury/pages/overview/value-card/value-card.ts
@@ -1,6 +1,12 @@
 import './value-card.scss';
+import { I18N } from '@aurelia/i18n';
 import { ICustomElementViewModel, customElement } from 'aurelia';
 import template from './value-card.html';
 
 @customElement({ name: 'value-card', template })
-export class ValueCard implements ICustomElementViewModel {}
+export class ValueCard implements ICustomElementViewModel {
+  public tooltip = '';
+  constructor(@I18N private readonly i18n: I18N) {
+    this.tooltip = this.i18n.tr('navigation.treasury.overview.value.tooltip');
+  }
+}
