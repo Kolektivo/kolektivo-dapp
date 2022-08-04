2a7732263cef0a20db7799b3003c6c030fa3a587
Doug Kent <github@douglaskent.com>
2022-08-04 16:37:39 -0600

    add environment variables

--- src/environment-variables.ts
+++ src/environment-variables.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { AllowedNetworks } from './services';
 
 export const isDev = process.env.NODE_ENV === 'development';
 export const ethereumNetwork = process.env.NETWORK as AllowedNetworks | undefined;
+export const COINGECKO_API_KEY = process.env.COINGECKO_API_KEY!;
+export const IPFS_GATEWAY = process.env.IPFS_GATEWAY!;
+export const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY!;
