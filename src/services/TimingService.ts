433cf2214adc4f3435ab5d41c79af3762fab916d
Doug Kent <github@douglaskent.com>
2022-08-23 03:23:21 -0600

    use isDev in TimingService

--- src/services/TimingService.ts
+++ src/services/TimingService.ts
@@ -1,14 +1,15 @@
+import { isDev } from './../environment-variables';
 /* eslint-disable no-console */
 import { EthereumService } from './ethereum-service';
 
 export function startTimer(label: string): void {
-  if (EthereumService.targetedNetwork !== 'Celo' || process.env.NODE_ENV === 'development') {
+  if (EthereumService.targetedNetwork !== 'Celo' || isDev) {
     console.time(label);
   }
 }
 
 export function endTimer(label: string): void {
-  if (EthereumService.targetedNetwork !== 'Celo' || process.env.NODE_ENV === 'development') {
+  if (EthereumService.targetedNetwork !== 'Celo' || isDev) {
     console.timeEnd(label);
   }
 }
