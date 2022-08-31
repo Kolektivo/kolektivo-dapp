98f0c9572bee26dcd11362de3f44858151dd0fa8
Brandon Seydel <brandonseydel@hotmail.com>
2022-08-31 07:34:12 -0500

    fix missing paranthesis...

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      plugins: [nodePolyfills],
+      plugins: [nodePolyfills()],
     },
     commonjsOptions: {
       transformMixedEsModules: true,
