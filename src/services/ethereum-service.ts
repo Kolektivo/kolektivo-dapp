cce75be52ccecdd6c520c0cd76e5dc6719b89b03
Doug Kent <github@douglaskent.com>
2022-09-08 12:29:12 -0600

    Alfa with an A

--- src/services/ethereum-service.ts
+++ src/services/ethereum-service.ts
@@ -90,10 +90,10 @@ export class EthereumService {
   public readonly endpoints: Record<AllowedNetworks, string> = {
     // Celo: `https://forno.celo.org`,
     Celo: 'https://celo.rpcs.dev:8545',
-    // alfajores: `https://e761db8d40ea4f95a10923da3ffa47a3.eth.rpc.rivet.cloud/`,
+    // Alfajores: `https://e761db8d40ea4f95a10923da3ffa47a3.eth.rpc.rivet.cloud/`,
     Alfajores: `https://alfajores.rpcs.dev:8545`,
-    // alfajores: `https://alfajores-forno.celo-testnet.org`,
-    // alfajores: `https://celo-alfajores-rpc.allthatnode.com`,
+    // Alfajores: `https://alfajores-forno.celo-testnet.org`,
+    // Alfajores: `https://celo-alfajores-rpc.allthatnode.com`,
   };
 
   private readonly providerOptions = {
