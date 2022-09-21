3f3412752fba2ddb204acf25d799e88d41985e47
Chad Cartwright <vipergtsrz@gmail.com>
2022-09-21 08:38:44 -0500

    Fix linting errors for now until Mihir gets back to us with the badge mapping

--- src/models/badge-type.ts
+++ src/models/badge-type.ts
@@ -1,4 +1,12 @@
 export enum BadgeType {
+  Local_Kolektivo_Multi_Sig_Member = 0,
+  Reserve_Arbitrageur = 2,
+  Treasury_Arbitrageur = 3,
+  Topology_Data_Delegate = 4,
+  Ecology_Data_Delegate = 5,
+  Treasury_Delegate = 6,
+  Reserve_Monetary_Delegate = 7,
+  Kolektivo_Network_Multi_Sig_Member = 8,
   MONETARY_CONTROLLER = 1,
   MONETARY_DELEGATE = 100,
   VETO_DELEGATE = 101,
