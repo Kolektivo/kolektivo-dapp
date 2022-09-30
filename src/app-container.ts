f5dbfd521eaaf45a896aa178867bca1aab7ba183
Brandon Seydel <brandonseydel@hotmail.com>
2022-09-30 11:32:24 -0500

    change to http client for testing wiht marvin ipfs

--- src/app-container.ts
+++ src/app-container.ts
@@ -10,7 +10,7 @@ import { RouterConfiguration } from '@aurelia/router';
 import { Services } from './services/services';
 import { StandardConfiguration } from '@aurelia/runtime-html';
 import { Store } from './stores';
-import { create } from 'ipfs-core';
+import { create } from 'ipfs-http-client';
 import { imageMap } from './app-images';
 import { isDev } from './environment-variables';
 import designScss from './design-system/styles/shared.scss';
@@ -18,16 +18,17 @@ import en from './locales/en/translation.json';
 import intervalPlural from 'i18next-intervalplural-postprocessor';
 import scss from './shared.scss';
 
-const ipfs = await create({
-  repo: String(Math.random() + Date.now()),
-  init: { algorithm: 'Ed25519' },
-});
-
-// const ipfs = create({
-//   headers: { 'x-kol-auth': '1af5b9c6-a8ee-44ef-b211-9d4bf66e1ab8' },
-//   port: 5001,
-//   url: 'https://ipfs.rpcs.dev:5001',
+// const ipfs = await create({
+//   repo: String(Math.random() + Date.now()),
+//   init: { algorithm: 'Ed25519' },
 // });
+
+const ipfs = create({
+  headers: { 'x-kol-auth': '1af5b9c6-a8ee-44ef-b211-9d4bf66e1ab8' },
+  port: 5001,
+  url: 'https://ipfs.rpcs.dev:5001',
+});
+await ipfs.id();
 
 export const appContainer: IContainer = DI.createContainer()
   .register(
