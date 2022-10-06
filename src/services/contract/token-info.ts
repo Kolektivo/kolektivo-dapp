47fc2e66ba0f2390f8cc3208ec3d085b17247701
Brandon Seydel <brandonseydel@hotmail.com>
2022-10-06 12:50:08 -0500

    Change the provider to be correct since default provider is not compat with celo/alf

--- src/services/contract/token-info.ts
+++ src/services/contract/token-info.ts
@@ -1,11 +1,11 @@
+import { CeloProvider } from '@celo-tools/celo-ethers-wrapper';
 import { ITokenInfo, ITokenListUniswap } from 'services/token-types';
-import { getDefaultProvider } from 'ethers';
 import { isCelo } from 'environment-variables';
 
 const tokenListUri = 'https://cdn.jsdelivr.net/gh/Kolektivo/tokenlists@main/tokenlist.json';
 const endpoint = isCelo ? 'https://celo.rpcs.dev:8545' : `https://alfajores.rpcs.dev:8545`;
 
-export const defaultProvider = getDefaultProvider(endpoint);
+export const defaultProvider = new CeloProvider(endpoint);
 
 export const tokenInfos =
   typeof window !== 'undefined'
