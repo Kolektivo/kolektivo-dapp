e626de053ece82426924586e8a865c23a3d3eb07
Brandon Seydel <brandonseydel@hotmail.com>
2022-10-20 10:29:41 -0500

    refac

--- src/stores/block-chain-store.ts
+++ src/stores/block-chain-store.ts
@@ -9,11 +9,49 @@ export type IBlockChainStore = BlockChainStore;
 export const IBlockChainStore = DI.createInterface<IBlockChainStore>('BlockChainStore');
 
 export class BlockChainStore {
+  public provider?: Web3Provider;
+  public walletProvider?: Web3Provider;
+
   constructor(
     @IEthereumService private readonly ethereumService: IEthereumService,
     @IConfiguration private readonly configuration: IConfiguration,
     @IAccountStore private readonly accountStore: IAccountStore,
-  ) {}
+  ) {
+    void this.autoConnect();
+  }
+
+  private async autoConnect() {
+    const provider = await this.ethereumService.getMetaMaskProvider();
+    provider && void this.connect(provider as unknown as Web3Provider);
+  }
+
+  private addListeners() {
+    if (!this.provider) return;
+    this.provider.on('accountsChanged', this.handleAccountsChanged);
+    this.provider.on('chainChanged', this.handleChainChanged);
+    this.provider.on('disconnect', this.handleDisconnect);
+  }
+
+  private removeListeners() {
+    if (!this.provider) return;
+    this.provider.removeListener('accountsChanged', this.handleAccountsChanged);
+    this.provider.removeListener('chainChanged', this.handleChainChanged);
+    this.provider.removeListener('disconnect', this.handleDisconnect);
+  }
+
+  private handleDisconnect = () => {
+    this.walletProvider = undefined;
+    this.accountStore.walletAddress = undefined;
+  };
+
+  private handleChainChanged = (chainId: number) => {
+    alert(chainId);
+  };
+
+  private handleAccountsChanged = (accounts?: string[]) => {
+    this.accountStore.walletAddress = accounts?.[0];
+    void this.accountStore.loadBadges();
+  };
 
   public static register(container: IContainer): void {
     container.register(Registration.singleton(IBlockChainStore, BlockChainStore));
@@ -27,9 +65,12 @@ export class BlockChainStore {
     return !!this.accountStore.walletAddress;
   }
 
-  public async connect() {
-    const provider = await this.ethereumService.connect();
-    this.accountStore.web3Provider = new Web3Provider(provider);
+  public async connect(web3Provider?: Web3Provider) {
+    const provider = web3Provider ?? (await this.ethereumService.connect());
+    this.removeListeners();
+    this.provider = provider;
+    this.addListeners();
+    void this.accountStore.connect(this.provider);
   }
 
   public connectKolektivoWallet(): void {
@@ -40,12 +81,14 @@ export class BlockChainStore {
     return this.ethereumService.getMetaMaskProvider();
   }
 
-  public switchToTargetedNetwork(walletProvider: Web3Provider) {
-    return this.ethereumService.switchToTargetedNetwork(walletProvider);
+  public switchToTargetedNetwork() {
+    if (!this.walletProvider) return;
+    return this.ethereumService.switchToTargetedNetwork(this.walletProvider);
   }
 
   public disconnect(): void {
-    return (this.accountStore.walletAddress = undefined);
+    void this.accountStore.connect(undefined);
+    this.provider = undefined;
   }
 
   public getEtherscanLink(addressOrHash: string, tx = false): string {
