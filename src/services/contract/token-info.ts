c9c0d312b19aa97a1535c2bd9610252db06ae88b
Brandon Seydel <brandonseydel@hotmail.com>
2022-10-24 07:34:39 -0500

    fix fetch

--- src/services/contract/token-info.ts
+++ src/services/contract/token-info.ts
@@ -6,8 +6,9 @@ const tokenListUri = 'https://cdn.jsdelivr.net/gh/Kolektivo/tokenlists@main/toke
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 export const getTokenInfos = () =>
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  fetch
-    ? fetch(tokenListUri, { method: 'GET', headers: { accept: 'application/json' } })
+  globalThis.fetch
+    ? globalThis
+        .fetch(tokenListUri, { method: 'GET', headers: { accept: 'application/json' } })
         .then(async (y) => (await y.json()) as ITokenListUniswap)
         .then((y) => y.tokens as ITokenInfo[])
     : [];
