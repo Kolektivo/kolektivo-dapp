b04daa2eeed09eba3eadead3d83636154da964ee
Brandon Seydel <brandonseydel@hotmail.com>
2022-11-20 20:16:49 -0600

    push 404 and some fixes for staging

--- src/pages/not-found/not-found.ts
+++ src/pages/not-found/not-found.ts
@@ -1,10 +1,22 @@
-import { customElement } from 'aurelia';
+import { customElement, ICustomElementViewModel } from 'aurelia';
 
 import template from './not-found.html';
+
+import './not-found.scss';
+
 @customElement({ name: 'not-found', template })
-export class NotFound {
+export class NotFound implements ICustomElementViewModel {
   public static parameters = ['id'];
   public missingComponent?: string;
+  promise?: Promise<unknown>;
+
+  binding() {
+    this.promise = import('@lottiefiles/lottie-player');
+  }
+
+  async attaching() {
+    await this.promise;
+  }
 
   public load(parameters: { id: string }): void {
     this.missingComponent = parameters.id;
