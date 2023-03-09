583abdbd2f9efe0a49c95f3e041f082f15fb47d9
Brandon Seydel <brandonseydel@hotmail.com>
2023-03-09 09:01:36 -0600

    add explicit option for storybook so it shows on DEV with a PROD build

--- src/environment-variables.ts
+++ src/environment-variables.ts
@@ -3,6 +3,7 @@ export const IPFS_GATEWAY = import.meta.env.KOL_IPFS_GATEWAY;
 export const CHAIN_ID = Number(import.meta.env.KOL_CHAIN_ID ?? 42220);
 export const CHAIN_URL = import.meta.env.KOL_CHAIN_URL ?? 'https://forno.celo.org';
 export const FIREBASE_API_KEY = import.meta.env.KOL_FIREBASE_API_KEY;
+export const SHOW_STORYBOOK = Boolean(import.meta.env.KOL_SHOW_STORYBOOK);
 export const CHAIN = import.meta.env.KOL_CHAIN ?? 'Celo';
 export const SCAN_LINK = import.meta.env.KOL_SCAN_LINK ?? 'https://explorer.celo.org/{type}/{address}';
 export const IS_TESTING = Boolean(import.meta.env.KOL_TESTING ?? true);
