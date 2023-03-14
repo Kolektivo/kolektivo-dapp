110db4f04cdf4bc5a7f4b1ffe604112bc54aa582
Chad Cartwright <vipergtsrz@gmail.com>
2023-03-14 10:03:34 -0500

    change the e2e test to look at reserve instead of treasury

--- src/__e2e__/meow.spec.ts
+++ src/__e2e__/meow.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 const BASE_URL = process.env.URL ?? 'http://localhost:9000/';
 
@@ -6,9 +6,7 @@ test.beforeEach(async ({ page }) => {
   await page.goto(BASE_URL);
 });
 
-// test('test', async ({ page }) => {
-//   await page.getByRole('link', { name: 'Treasury' }).click();
-//   expect(page.locator('k-text:has-text("Current Price") span')).not.toBeNull();
-//   expect(page.locator('k-text:has-text("Kolektivo Treasury Token (KTT)") span')).not.toBeNull();
-//   expect(page.locator('k-text:has-text("Kolektivo Network Treasury") span')).not.toBeNull();
-// });
+test('test', async ({ page }) => {
+  await page.getByRole('link', { name: 'Reserve' }).click();
+  expect(page.locator('k-text:has-text("Reserve Value") span')).not.toBeNull();
+});
