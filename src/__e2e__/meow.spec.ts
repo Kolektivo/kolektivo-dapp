74dd8a819a180182b9fa660ddf698a3c8746321d
Chad Cartwright <vipergtsrz@gmail.com>
2023-03-14 09:28:37 -0500

    Remove e2e test that expected treasury to be there

--- src/__e2e__/meow.spec.ts
+++ src/__e2e__/meow.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
 const BASE_URL = process.env.URL ?? 'http://localhost:9000/';
 
@@ -6,9 +6,9 @@ test.beforeEach(async ({ page }) => {
   await page.goto(BASE_URL);
 });
 
-test('test', async ({ page }) => {
-  await page.getByRole('link', { name: 'Treasury' }).click();
-  expect(page.locator('k-text:has-text("Current Price") span')).not.toBeNull();
-  expect(page.locator('k-text:has-text("Kolektivo Treasury Token (KTT)") span')).not.toBeNull();
-  expect(page.locator('k-text:has-text("Kolektivo Network Treasury") span')).not.toBeNull();
-});
+// test('test', async ({ page }) => {
+//   await page.getByRole('link', { name: 'Treasury' }).click();
+//   expect(page.locator('k-text:has-text("Current Price") span')).not.toBeNull();
+//   expect(page.locator('k-text:has-text("Kolektivo Treasury Token (KTT)") span')).not.toBeNull();
+//   expect(page.locator('k-text:has-text("Kolektivo Network Treasury") span')).not.toBeNull();
+// });
